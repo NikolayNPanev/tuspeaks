5672ec9705a6b413d98a37f724db60a4dcc697f5
NikolayNPanev <nikolay.n.panev@gmail.com>
2023-04-19 17:14:08 +0300

    Make navbar lookable

--- index.php
+++ index.php
@@ -2,3 +2,41 @@
 include("DBphp/CreateDB.php");
 //CreateDB();
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="CSS/style.css">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title></title>
+</head>
+
+
+<body id="body">
+<div class="navbar">
+<ul>
+  <h1>TU Speaks</h1>
+  <div class="navmenu">
+  <li><a>...</a></li>
+  <li><a href="#body">Home</a></li>
+  <li><a href="#body">News</a></li>
+  <li><a href="#body">Contact</a></li>
+  <li><a href="#body">About</a></li>
+  </div>
+</ul>
+</div>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam non mollis velit. Maecenas mauris mauris, suscipit sit amet arcu at, cursus convallis orci. In aliquet leo a consequat feugiat. Pellentesque placerat nibh at elit cursus rhoncus. Ut vulputate magna eget tellus ultrices ornare. Nullam massa velit, fermentum sit amet arcu eget, pharetra tempus dolor. Maecenas diam erat, auctor et sapien ut, ultrices vulputate lorem. In diam elit, vehicula ac pulvinar non, interdum sit amet lorem. Quisque arcu metus, dapibus at ultricies eu, volutpat at purus. Nam metus tellus, facilisis vitae tellus lobortis, facilisis euismod tortor. Quisque urna ante, blandit non sem ut, pulvinar euismod dolor.</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam non mollis velit. Maecenas mauris mauris, suscipit sit amet arcu at, cursus convallis orci. In aliquet leo a consequat feugiat. Pellentesque placerat nibh at elit cursus rhoncus. Ut vulputate magna eget tellus ultrices ornare. Nullam massa velit, fermentum sit amet arcu eget, pharetra tempus dolor. Maecenas diam erat, auctor et sapien ut, ultrices vulputate lorem. In diam elit, vehicula ac pulvinar non, interdum sit amet lorem. Quisque arcu metus, dapibus at ultricies eu, volutpat at purus. Nam metus tellus, facilisis vitae tellus lobortis, facilisis euismod tortor. Quisque urna ante, blandit non sem ut, pulvinar euismod dolor.</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam non mollis velit. Maecenas mauris mauris, suscipit sit amet arcu at, cursus convallis orci. In aliquet leo a consequat feugiat. Pellentesque placerat nibh at elit cursus rhoncus. Ut vulputate magna eget tellus ultrices ornare. Nullam massa velit, fermentum sit amet arcu eget, pharetra tempus dolor. Maecenas diam erat, auctor et sapien ut, ultrices vulputate lorem. In diam elit, vehicula ac pulvinar non, interdum sit amet lorem. Quisque arcu metus, dapibus at ultricies eu, volutpat at purus. Nam metus tellus, facilisis vitae tellus lobortis, facilisis euismod tortor. Quisque urna ante, blandit non sem ut, pulvinar euismod dolor.</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam non mollis velit. Maecenas mauris mauris, suscipit sit amet arcu at, cursus convallis orci. In aliquet leo a consequat feugiat. Pellentesque placerat nibh at elit cursus rhoncus. Ut vulputate magna eget tellus ultrices ornare. Nullam massa velit, fermentum sit amet arcu eget, pharetra tempus dolor. Maecenas diam erat, auctor et sapien ut, ultrices vulputate lorem. In diam elit, vehicula ac pulvinar non, interdum sit amet lorem. Quisque arcu metus, dapibus at ultricies eu, volutpat at purus. Nam metus tellus, facilisis vitae tellus lobortis, facilisis euismod tortor. Quisque urna ante, blandit non sem ut, pulvinar euismod dolor.</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam non mollis velit. Maecenas mauris mauris, suscipit sit amet arcu at, cursus convallis orci. In aliquet leo a consequat feugiat. Pellentesque placerat nibh at elit cursus rhoncus. Ut vulputate magna eget tellus ultrices ornare. Nullam massa velit, fermentum sit amet arcu eget, pharetra tempus dolor. Maecenas diam erat, auctor et sapien ut, ultrices vulputate lorem. In diam elit, vehicula ac pulvinar non, interdum sit amet lorem. Quisque arcu metus, dapibus at ultricies eu, volutpat at purus. Nam metus tellus, facilisis vitae tellus lobortis, facilisis euismod tortor. Quisque urna ante, blandit non sem ut, pulvinar euismod dolor.</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam non mollis velit. Maecenas mauris mauris, suscipit sit amet arcu at, cursus convallis orci. In aliquet leo a consequat feugiat. Pellentesque placerat nibh at elit cursus rhoncus. Ut vulputate magna eget tellus ultrices ornare. Nullam massa velit, fermentum sit amet arcu eget, pharetra tempus dolor. Maecenas diam erat, auctor et sapien ut, ultrices vulputate lorem. In diam elit, vehicula ac pulvinar non, interdum sit amet lorem. Quisque arcu metus, dapibus at ultricies eu, volutpat at purus. Nam metus tellus, facilisis vitae tellus lobortis, facilisis euismod tortor. Quisque urna ante, blandit non sem ut, pulvinar euismod dolor.</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam non mollis velit. Maecenas mauris mauris, suscipit sit amet arcu at, cursus convallis orci. In aliquet leo a consequat feugiat. Pellentesque placerat nibh at elit cursus rhoncus. Ut vulputate magna eget tellus ultrices ornare. Nullam massa velit, fermentum sit amet arcu eget, pharetra tempus dolor. Maecenas diam erat, auctor et sapien ut, ultrices vulputate lorem. In diam elit, vehicula ac pulvinar non, interdum sit amet lorem. Quisque arcu metus, dapibus at ultricies eu, volutpat at purus. Nam metus tellus, facilisis vitae tellus lobortis, facilisis euismod tortor. Quisque urna ante, blandit non sem ut, pulvinar euismod dolor.</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam non mollis velit. Maecenas mauris mauris, suscipit sit amet arcu at, cursus convallis orci. In aliquet leo a consequat feugiat. Pellentesque placerat nibh at elit cursus rhoncus. Ut vulputate magna eget tellus ultrices ornare. Nullam massa velit, fermentum sit amet arcu eget, pharetra tempus dolor. Maecenas diam erat, auctor et sapien ut, ultrices vulputate lorem. In diam elit, vehicula ac pulvinar non, interdum sit amet lorem. Quisque arcu metus, dapibus at ultricies eu, volutpat at purus. Nam metus tellus, facilisis vitae tellus lobortis, facilisis euismod tortor. Quisque urna ante, blandit non sem ut, pulvinar euismod dolor.</p>
+
+</body>
+</html>
+   
+
+
